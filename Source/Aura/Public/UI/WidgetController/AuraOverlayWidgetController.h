6aa5baac586dff752145b804ca5daa92c2932754
Kwon Mingyu <kmgmg230601@gmail.com>
2024-02-17 00:42:08 +0900

    어느정도 이해해서 마나도 추가했다

--- Source/Aura/Public/UI/WidgetController/AuraOverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraOverlayWidgetController.h
@@ -9,6 +9,8 @@
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
 
 /**
  * 
@@ -23,14 +25,31 @@ public:
 	virtual void BindCallbacksToDependencies() override;
 
 
+	// 지금 흐름을 보면 
+	// 1. 먼저 속성이 변경
+	// 2. controller가 속성이 변경 된 것을 delegate로 받아서 알아차림
+	// 3. widget은 controller에서 값이 변경 된 것을 delegate로 받아서 알아차림
+	// 종속성은 속성 <= controller <= widget 이렇게 되어있기에 이벤트 받는 것을 2번 해야 비로소 
+	// 속성이 바뀐 것으로 위젯이 변하게 하는 작업이 완료 된다
+
+	// Begin  위젯이 컨트롤러에 등록할 signature
 	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
 	FOnHealthChangedSignature OnHealthChanged;
 
 	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
 	FOnHealthChangedSignature OnMaxHealthChanged;
 
+	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+	FOnManaChangedSignature OnManaChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+	FOnMaxManaChangedSignature OnMaxManaChanged;
+	// End  위젯이 컨트롤러에 등록할 signature
+
 protected:
 	void HealthChanged(const FOnAttributeChangeData& Data) const;
 	void MaxHealthChanged(const FOnAttributeChangeData& Data) const;
+	void ManaChanged(const FOnAttributeChangeData& Data) const;
+	void MaxManaChanged(const FOnAttributeChangeData& Data) const;
 
 };
