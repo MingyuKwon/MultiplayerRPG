94c1d7b66c97a69fc0f41b4cb2ae86da8b42ad4e
Kwon Mingyu <kmgmg230601@gmail.com>
2024-02-17 20:19:36 +0900

    preAttributeChange는 값이 설정 되기 이전에attribute set에서  호출되는 함수이다. 여기선 가능하면 다른 로직을 쓰지말고 clamp 부분만 하고, 실제 값이 변할 때 마다 그 트리거로 뭐가 발생시키고 싶다면 이후에 나올 postAttributeChange 에서 하도록 하자

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -26,6 +26,22 @@ void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Ou
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
 }
 
+// Good for Clamping
+void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0 , GetMaxHealth());
+	}
+
+	if (Attribute == GetManaAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0, GetMaxMana());
+	}
+}
+
 void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
 {
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldHealth);
