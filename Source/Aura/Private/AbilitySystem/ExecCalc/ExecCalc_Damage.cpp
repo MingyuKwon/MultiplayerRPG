02a79de92bc8aee1d08a299d5a4e2e42d3dd7aea
Kwon Mingyu <kmgmg230601@gmail.com>
2024-02-28 22:10:56 +0900

    capture을 하는 방식은 어떤 속성을 캡쳐 가능한 상태로 미리 구조체로 선언 해 두고, 그 속성을 calc의 생성자에서 등록시켜 놓는 것이다. 그리고 계산 할 때 쯔음 되면 자동으로 등록 해 놓은 속성들은 자동으로 캡쳐가 되고, 그 캡쳐 값을 꺼내와서 사용하면 되는 것

--- Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -3,10 +3,34 @@
 
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 #include "AbilitySystemComponent.h"
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+
+struct AuraDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
+
+	AuraDamageStatics()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, BlockChance, Target, false);
+	}
+
+};
+
+static const AuraDamageStatics& DamageStatic()
+{
+	// static 변수는 함수가 종료되도 사라지지 않고 오로지 1개만 존재한다
+	static AuraDamageStatics DStatics;
+	return DStatics;
+}
 
 UExecCalc_Damage::UExecCalc_Damage()
 {
-
+	RelevantAttributesToCapture.Add(DamageStatic().ArmorDef);
+	RelevantAttributesToCapture.Add(DamageStatic().BlockChanceDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, 
@@ -21,4 +45,28 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
 
 
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	// Get Damage Set bu caller magnitute
+	float Damage = Spec.GetSetByCallerMagnitude(FAuraGamplayTags::Get().Damage);
+
+	// Caputure BlockCHance on Target, and determine if ther was a successful block
+	// if block, halve the damage
+
+	float TargetBlockChance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatic().BlockChanceDef, EvaluationParameters, TargetBlockChance);
+	TargetBlockChance = FMath::Max(0.f, TargetBlockChance);
+
+	const bool bBlocked = FMath::RandRange(1, 100) < TargetBlockChance;
+	if (bBlocked)
+	{
+		Damage = Damage / 2.f;
+	}
+
+	const FGameplayModifierEvaluatedData EvaluateData(UAuraAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
+	OutExecutionOutput.AddOutputModifier(EvaluateData);
 }
